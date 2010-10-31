bf40e4c0cae569748f2e6e0e15767b670cb4656d
Nicholas Mayne <Jetski5822@gmail.com>
2010-10-31 18:03:37 +0000

    Initial Check in

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace NGM.OpenAuthentication.Controllers
+{
+    public class AccountController : Controller {
+        public ActionResult OpenIdLogOn() {
+            return View();
+        }
+
+        [HttpPost, ActionName("OpenIdLogOn")]
+        public ActionResult _OpenIdLogOn() {
+            return new RedirectResult("https://www.google.com/accounts/ServiceLogin");
+        }
+    }
+}
---
+++ Routes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace NGM.OpenAuth {
+    public class Routes : IRouteProvider {
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new[] {
+                             new RouteDescriptor {
+                                                     Route = new Route(
+                                                         "OpenIdLogOn",
+                                                         new RouteValueDictionary {
+                                                                                      {"area", "NGM.OpenAuth"},
+                                                                                      {"controller", "Account"},
+                                                                                      {"action", "OpenIdLogOn"}
+                                                                                  },
+                                                         new RouteValueDictionary(),
+                                                         new RouteValueDictionary {
+                                                                                      {"area", "NGM.OpenAuth"}
+                                                                                  },
+                                                         new MvcRouteHandler())
+                                                 },
+                         };
+        }
+    }
+}
