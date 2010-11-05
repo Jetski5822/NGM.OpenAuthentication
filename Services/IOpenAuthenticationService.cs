3fdf312290c238f331a59c2abade98d5299d0e92
Nicholas Mayne <Jetski5822@gmail.com>
2010-11-05 19:20:21 +0000

    moved public method up to interface on the authenticationservice.

--- Services/IOpenAuthenticationService.cs
+++ Services/IOpenAuthenticationService.cs
@@ -1,8 +1,10 @@
 ﻿using NGM.OpenAuthentication.Core;
+using NGM.OpenAuthentication.Models;
 using Orchard.Security;
 
 namespace NGM.OpenAuthentication.Services {
     public interface IOpenAuthenticationService {
         IUser GetUserFor(OpenIdIdentifier openIdIdentifier);
+        void AssociateOpenIdWithUser(IUser user, OpenIdIdentifier openIdIdentifier, ExtendedUserPropertiesContext extendedUserPropertiesContext)
     }
 }
