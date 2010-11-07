6e81a2aea4d288a765ea457705c9e05c9f7a2b2a
Nicholas Mayne <Jetski5822@gmail.com>
2010-11-07 15:53:21 +0000

    Hooking up GetUser.. Attempted to write tests for get user to return a user, when a record existed for identifier, but orchards code in certain areas are difficult to mock out.

--- Services/OpenAuthenticationService.cs
+++ Services/OpenAuthenticationService.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using NGM.OpenAuthentication.Models;
+using Orchard.ContentManagement;
+using Orchard.Data;
 using Orchard.Security;
 
 namespace NGM.OpenAuthentication.Services {
     public class OpenAuthenticationService : IOpenAuthenticationService {
+        private readonly IContentManager _contentManager;
+        private readonly IRepository<OpenAuthenticationPartRecord> _openAuthenticationPartRecord;
+
+        public OpenAuthenticationService(IContentManager contentManager, IRepository<OpenAuthenticationPartRecord> openAuthenticationPartRecord) {
+            _contentManager = contentManager;
+            _openAuthenticationPartRecord = openAuthenticationPartRecord;
+        }
 
         public void AssociateOpenIdWithUser(IUser user, string openIdIdentifier) {
             throw new NotImplementedException();
@@ -13,6 +23,12 @@ namespace NGM.OpenAuthentication.Services {
         }
 
         public IUser GetUser(string openIdIdentifier) {
+            var record = _openAuthenticationPartRecord.Get(o => o.Identifier == openIdIdentifier);
+
+            if (record != null) {
+                return _contentManager.Get<IUser>(record.Id);
+            }
+
             return null;
         }
 
--- Tests/NGM.OpenAuthentication.Tests/UnitTests/OpenauthenticationServiceTests.cs
+++ Tests/NGM.OpenAuthentication.Tests/UnitTests/OpenauthenticationServiceTests.cs
@@ -1,19 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Moq;
+using NGM.OpenAuthentication.Models;
 using NGM.OpenAuthentication.Services;
 using NUnit.Framework;
+using Orchard.Data;
 
 namespace NGM.OpenAuthentication.Tests.UnitTests {
     [TestFixture]
     public class OpenAuthenticationServiceTests {
         [Test]
         public void should_return_null_when_no_user_exists_for_identifier() {
-            var openAuthenticationService = new OpenAuthenticationService();
+            var mockRepository = new Mock<IRepository<OpenAuthenticationPartRecord>>();
+
+            var openAuthenticationService = new OpenAuthenticationService(null, mockRepository.Object);
             var user = openAuthenticationService.GetUser("test");
 
             Assert.That(user, Is.Null);
+        }
+
+        [Test]
+        [Ignore ("Cant seem to mock the stuff i want to call. I will do this test with a functional test")]
+        public void should_return_user_when_identifier_exists_against_user() {
+            //var identifier = "foo";
+            //var openAuthenticationPartRecord = new OpenAuthenticationPartRecord {Id = 123, Identifier = identifier};
+
+            //var mockRepository = new Mock<IRepository<OpenAuthenticationPartRecord>>();
+            //mockRepository.Setup(ctx => ctx.Get(It.IsAny<Expression<Func<OpenAuthenticationPartRecord, bool>>>())).Returns(openAuthenticationPartRecord);
+
+            //var mockUser = new Mock<IUser>();
+
+            //var mockContentItem = new Mock<ContentItem>();
+            //mockContentItem.Setup(ctx => ctx.Get(It.IsAny<Type>())).Returns(mockUser.Object);
+
+            //var mockContentManager = new Mock<IContentManager>();
+            //mockContentManager.Setup(ctx => ctx.Get(openAuthenticationPartRecord.Id)).Returns(mockContentItem.Object);
+
+            //var openAuthenticationService = new OpenAuthenticationService(mockContentManager.Object, mockRepository.Object);
+            //var user = openAuthenticationService.GetUser(identifier);
+
+            //Assert.That(user, Is.Not.Null);
         }
     }
 }
