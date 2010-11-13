3b2e67f809391bd1696e88d318a67ae2ff164bb0
Nicholas Mayne <Jetski5822@gmail.com>
2010-11-13 13:04:54 +0000

    Changed Part to nolonger be a sealed class

--- Models/OpenAuthenticationPart.cs
+++ Models/OpenAuthenticationPart.cs
@@ -1,7 +1,7 @@
 ﻿using Orchard.ContentManagement;
 
 namespace NGM.OpenAuthentication.Models {
-    public sealed class OpenAuthenticationPart : ContentPart<OpenAuthenticationPartRecord> {
+    public class OpenAuthenticationPart : ContentPart<OpenAuthenticationPartRecord> {
         public string Identifier {
             get { return Record.Identifier; }
             set { Record.Identifier = value; }
