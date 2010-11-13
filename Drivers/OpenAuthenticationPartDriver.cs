6793d2986cde9f2aa6c4c0bd73703b657209b9d2
Nicholas Mayne <Jetski5822@gmail.com>
2010-11-13 13:05:27 +0000

    Added a Driver as you cant use the .As<> statement withour one for you specific part record.

---
+++ Drivers/OpenAuthenticationPartDriver.cs
@@ -0,0 +1,12 @@
+﻿using NGM.OpenAuthentication.Models;
+using Orchard.ContentManagement.Drivers;
+
+namespace NGM.OpenAuthentication.Drivers {
+    public class OpenAuthenticationPartDriver : ContentPartDriver<OpenAuthenticationPart> {
+        protected override string Prefix {
+            get {
+                return "OpenAuthentication";
+            }
+        }
+    }
+}
