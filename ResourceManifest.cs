e01c1cffc6f70698f2e204658404697d09cf9a09
Nicholas Mayne <Jetski5822@gmail.com>
2010-11-21 12:49:52 +0000

    Fixing the loading of the account.css file.

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -3,8 +3,7 @@ using Orchard.UI.Resources;
 namespace NGM.OpenAuthentication {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
-            var manifest = builder.Add();
-            manifest.DefineStyle("Account").SetUrl("account.css");
+            builder.Add().DefineStyle("Account").SetUrl("account.css");
         }
     }
 }
