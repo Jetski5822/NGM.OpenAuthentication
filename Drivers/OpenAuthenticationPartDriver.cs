7b78598499abbef54703fc21d39b1a4b56976c4f
Nicholas Mayne <Jetski5822@gmail.com>
2010-12-14 21:46:01 +0000

    Fixing Lib issue. Broke Verified Accounts page.. attempting to move it to a shape.

--- Drivers/OpenAuthenticationPartDriver.cs
+++ Drivers/OpenAuthenticationPartDriver.cs
@@ -1,12 +1,45 @@
 ﻿using NGM.OpenAuthentication.Models;
+using NGM.OpenAuthentication.ViewModels;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+using Orchard.Security;
 
 namespace NGM.OpenAuthentication.Drivers {
     public class OpenAuthenticationPartDriver : ContentPartDriver<OpenAuthenticationPart> {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IAuthorizationService _authorizationService;
+        private const string TemplateName = "Parts/OpenAuthentication.VerifiedOpenIdAccounts";
+
+        public OpenAuthenticationPartDriver(IAuthenticationService authenticationService,
+            IAuthorizationService authorizationService) {
+            _authenticationService = authenticationService;
+            _authorizationService = authorizationService;
+            T = NullLocalizer.Instance;
+        }
+
         protected override string Prefix {
             get {
                 return "OpenAuthentication";
             }
+        }
+
+        public Localizer T { get; set; }
+
+        protected override DriverResult Editor(OpenAuthenticationPart userRolesPart, dynamic shapeHelper) {
+            // don't show editor without apply roles permission
+            if (!_authorizationService.TryCheckAccess(StandardPermissions.SiteOwner, _authenticationService.GetAuthenticatedUser(), userRolesPart))
+                return null;
+
+            return null;
+        }
+
+        protected override DriverResult Editor(OpenAuthenticationPart userRolesPart, IUpdateModel updater, dynamic shapeHelper) {
+            // don't apply editor without apply roles permission
+            if (!_authorizationService.TryCheckAccess(StandardPermissions.SiteOwner, _authenticationService.GetAuthenticatedUser(), userRolesPart))
+                return null;
+
+            return null;
         }
     }
 }
