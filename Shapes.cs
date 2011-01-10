258f4a4d280d9d223ec9719eb9ea9236262d27d9
Nicholas Mayne <Jetski5822@gmail.com>
2011-01-10 20:46:02 +0000

    Updating to inject the authentication shape on to the logon page..

---
+++ Shapes.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.DisplayManagement.Implementation;
+
+namespace NGM.OpenAuthentication {
+    public class Shapes : IShapeFactoryEvents {
+        public void Creating(ShapeCreatingContext context) {
+
+        }
+
+        public void Created(ShapeCreatedContext context) {
+            if (context.ShapeType == "LogOn") {
+                context.Shape.Metadata.Wrappers.Add("Wrappers_Account_LogOn");
+            }
+        }
+    }
+}
