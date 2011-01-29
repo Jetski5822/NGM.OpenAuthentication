f41813478c1f69f2e261dc981a9bffe5b81030e0
Nicholas Mayne <Jetski5822@gmail.com>
2011-01-29 13:45:00 +0000

    Added a RegistrationFilter to try and push a shape to the registration page.

---
+++ Filters/RegistrationFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Mvc.Filters;
+
+namespace NGM.OpenAuthentication.Filters {
+    public class RegistrationFilter : FilterProvider, IResultFilter {
+        public void OnResultExecuting(ResultExecutingContext filterContext) {
+            
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) {
+            
+        }
+    }
+}
