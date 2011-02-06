f8b41bfc166d1f01b960d4d165866be9b29cfdc7
Nicholas Mayne <Jetski5822@gmail.com>
2011-02-06 17:00:20 +0000

    thinking about how to chane the error reporting to another module.

---
+++ Filters/LogOnFilter.cs
@@ -0,0 +1,26 @@
+﻿//﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//﻿using NGM.OpenAuthentication.Models;
+//﻿using Orchard.Mvc.Filters;
+
+//namespace NGM.OpenAuthentication.Filters {
+//    public class LogOnFilter : FilterProvider, IResultFilter {
+//        public void OnResultExecuting(ResultExecutingContext filterContext) {
+//            if (!filterContext.RouteData.Values.ContainsValue("LogOn"))
+//                return;
+
+//            var errors = filterContext.Controller.TempData.Where(o => o.Key.StartsWith("error-")).ToArray();
+
+//            foreach (var error in errors.Where(error => filterContext.Controller.ViewData.ModelState.ContainsKey(error.Key))) {
+//                filterContext.Controller.ViewData.ModelState.AddModelError(error.Key, error.Value.ToString());
+//            }
+//        }
+
+//        public void OnResultExecuted(ResultExecutedContext filterContext) {
+            
+//        }
+//    }
+//}
