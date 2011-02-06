fd0593b8b0cb8dcea3162eb4b05ec1bfe532ae9f
Nicholas Mayne <Jetski5822@gmail.com>
2011-02-06 20:50:14 +0000

    Added openid selectors to the logon form. All working lovely.

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -5,6 +5,11 @@ namespace NGM.OpenAuthentication {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
             manifest.DefineStyle("Account").SetUrl("account.css");
+
+            manifest.DefineStyle("openIdStyles").SetUrl("openid.css").SetVersion("1.3b1");
+            manifest.DefineScript("openIdSelector").SetUrl("openid-jquery-1.3b1.js").SetVersion("1.3b1").SetDependencies("jQuery");
+            manifest.DefineScript("openIdLocalization").SetUrl("openid-en.js").SetDependencies("openIdSelector").SetVersion("1.3b1");
+
         }
     }
 }
