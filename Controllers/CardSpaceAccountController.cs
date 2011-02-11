b4499a4f2c38def5dfe901a0b261dae6e7b3f53d
Nicholas Mayne <Jetski5822@gmail.com>
2011-02-11 20:11:06 +0000

    Added CardSpaceAccountController and repopinted view to correct controller.

---
+++ Controllers/CardSpaceAccountController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Orchard.Localization;
+using Orchard.Themes;
+
+namespace NGM.OpenAuthentication.Controllers {
+    [Themed]
+    public class CardSpaceAccountController : Controller {
+
+        public CardSpaceAccountController() {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public ActionResult LogOn(string returnUrl) {
+            return View();
+        }
+    }
+}
