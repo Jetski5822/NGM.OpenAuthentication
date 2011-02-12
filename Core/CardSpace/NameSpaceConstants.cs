0bdf699003d2180c929c3ef91e69a6968fc12ffe
Nicholas Mayne <Jetski5822@gmail.com>
2011-02-12 17:16:29 +0000

    Fixed previous checkin.

---
+++ Core/CardSpace/NameSpaceConstants.cs
@@ -0,0 +1,5 @@
+﻿namespace NGM.OpenAuthentication.Core.CardSpace {
+    public static class NamespaceConstants {
+        public const string ClaimsNamespace = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/";
+    }
+}
