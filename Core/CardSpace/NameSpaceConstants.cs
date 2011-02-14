0a44c4785d2b34743b99c5305bf6112ef73ce1df
Nicholas Mayne <Jetski5822@gmail.com>
2011-02-14 22:16:32 +0000

    Moved building of claims to server side code.

--- Core/CardSpace/NameSpaceConstants.cs
+++ Core/CardSpace/NameSpaceConstants.cs
@@ -1,5 +1,13 @@
 ﻿namespace NGM.OpenAuthentication.Core.CardSpace {
     public static class NamespaceConstants {
         public const string ClaimsNamespace = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/";
+
+        public static string BuildClaimFullNamespaceUrl(string claimValue) {
+            string formattedClaimValue = claimValue;
+            if (claimValue.StartsWith("/"))
+                formattedClaimValue = claimValue.Remove(0, 1);
+
+            return string.Format("{0}{1}", ClaimsNamespace, formattedClaimValue);
+        }
     }
 }
