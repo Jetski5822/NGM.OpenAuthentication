1162e7bd68556dea606ed68b46018c7261d02ade
Nicholas Mayne <Jetski5822@gmail.com>
2011-03-05 10:36:09 +0000

    more ground work

---
+++ Core/OAuth/LiveIdProviderAuthorizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NGM.OpenAuthentication.Core.OAuth {
+    public class LiveIdProviderAuthorizer : IOAuthProviderAuthorizer {
+        public AuthorizeState Authorize(string returnUrl) {
+            throw new NotImplementedException();
+        }
+
+        public string ClientKeyIdentifier {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string ClientSecret {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsConsumerConfigured {
+            get { throw new NotImplementedException(); }
+        }
+
+        public OAuthProvider Provider {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
