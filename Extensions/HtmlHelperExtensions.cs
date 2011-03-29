cf8449841967019376a770c018101ba83b8406e6
Nicholas Mayne <Jetski5822@gmail.com>
2011-03-29 22:33:40 +0100

    small fix to index page.. now you know your provider type.

---
+++ Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using NGM.OpenAuthentication.Core.OAuth;
+
+namespace NGM.OpenAuthentication.Extensions {
+    public static class HtmlHelperExtensions {
+        public static string TranslateProvider(this HtmlHelper htmlHelper, int hashedProvider) {
+            if (hashedProvider == OAuthProvider.Facebook.ToString().GetHashCode()) {
+                return "Facebook";
+            }
+            if (hashedProvider == OAuthProvider.LiveId.ToString().GetHashCode()) {
+                return "Microsoft Connect";
+            }
+            if (hashedProvider == OAuthProvider.Twitter.ToString().GetHashCode()) {
+                return "Twitter";
+            }
+            if (hashedProvider == "OpenId".ToString().GetHashCode()) {
+                return "Open Id";
+            }
+            return "Unknown Provider";
+        }
+    }
+}
