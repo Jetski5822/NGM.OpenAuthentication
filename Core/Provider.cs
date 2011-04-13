94dca771daaac44dfe276c27cb15226257d864d5
Nicholas Mayne <Jetski5822@gmail.com>
2011-04-13 23:20:27 +0100

    Added providers missed from last checkin

---
+++ Core/Provider.cs
@@ -0,0 +1,15 @@
+﻿namespace NGM.OpenAuthentication.Core {
+    public enum Provider {
+        OpenId,
+        Twitter,
+        Facebook,
+        LiveId,
+        CardSpace
+    }
+
+    public static class ProviderHelpers {
+        public static int GetHashedProvider(Provider provider) {
+            return provider.ToString().GetHashCode();
+        }
+    }
+}
