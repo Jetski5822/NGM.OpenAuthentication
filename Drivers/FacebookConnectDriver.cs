6927ad1dbc76aa6acc390eda842fd0ccc46b997c
Nicholas Mayne <Jetski5822@gmail.com>
2011-04-29 19:54:09 +0100

    hooked up facebook connect part driver to render widget

---
+++ Drivers/FacebookConnectDriver.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using NGM.OpenAuthentication.Models;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+
+namespace NGM.OpenAuthentication.Drivers {
+    [UsedImplicitly]
+    [OrchardFeature("Facebook")]
+    public class FacebookConnectDriver : ContentPartDriver<FacebookConnectSignInPart> {
+        protected override DriverResult Display(FacebookConnectSignInPart part, string displayType, dynamic shapeHelper) {
+            return ContentShape("FacebookConnectSignIn", () => shapeHelper.FacebookConnectSignIn(Model: part));
+        }
+    }
+}
