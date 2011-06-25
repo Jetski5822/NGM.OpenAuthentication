b7250cbcada08da32b546bf50240d409ca82d0ca
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-25 12:38:35 +0100

    Adding localization to Microsoft Connect scripts.

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using Orchard;
 using Orchard.Environment.Extensions;
@@ -68,6 +69,41 @@ namespace NGM.OpenAuthentication {
 
         private static void RegisterDefaultLocalizedVersionOfOpenIdJavascriptFiles(Orchard.UI.Resources.ResourceManifest manifest) {
             manifest.DefineScript("openIdLocalization").SetUrl("openid-en.js").SetDependencies("openIdSelector").SetVersion("1.3b1");
+        }
+    }
+
+    [OrchardFeature("MicrosoftConnect")]
+    public class MicrosoftConnectManifest : IResourceManifestProvider {
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        public MicrosoftConnectManifest(IWorkContextAccessor workContextAccessor)
+        {
+            _workContextAccessor = workContextAccessor;
+        }
+
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+            RegisterLocalizedVersionOfMicrosoftConnectJavascriptFiles(manifest);
+        }
+
+        private void RegisterLocalizedVersionOfMicrosoftConnectJavascriptFiles(Orchard.UI.Resources.ResourceManifest manifest) {
+            string currentCultureName = _workContextAccessor.GetContext().CurrentSite.SiteCulture;
+
+            if (string.IsNullOrEmpty(currentCultureName)) {
+                RegisterDefaultLocalizedVersionOfMicrosoftConnectJavascriptFiles(manifest);
+            } else {
+                try {
+                    var ci = new CultureInfo(currentCultureName);
+                    manifest.DefineScript("MicrosoftLiveConnect").SetUrl(string.Format("https://js.live.net/v5.0/{0}/wl.js", ci.Name)).SetVersion("5.0");
+                }
+                catch (CultureNotFoundException) {
+                    RegisterDefaultLocalizedVersionOfMicrosoftConnectJavascriptFiles(manifest);
+                }
+            }
+        }
+
+        private static void RegisterDefaultLocalizedVersionOfMicrosoftConnectJavascriptFiles(Orchard.UI.Resources.ResourceManifest manifest) {
+            manifest.DefineScript("MicrosoftLiveConnect").SetUrl("https://js.live.net/v5.0/wl.js").SetVersion("5.0");
         }
     }
 }
