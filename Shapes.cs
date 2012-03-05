4701865e80fc5064f9d0df377d41f5d6a15afbd7
Nicholas Mayne <Jetski5822@gmail.com>
2012-03-05 20:47:13 +0000

    moving to IOrchardService instead of thru static context

--- Shapes.cs
+++ Shapes.cs
@@ -1,27 +1,33 @@
 ﻿using System;
-using System.Web;
+using Orchard;
 using Orchard.DisplayManagement.Implementation;
 
 namespace NGM.OpenAuthentication {
     public class Shapes : IShapeFactoryEvents {
+        private readonly IOrchardServices _orchardServices;
+
+        public Shapes(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+
         public void Creating(ShapeCreatingContext context) {
         }
 
         public void Created(ShapeCreatedContext context) {
             if (context.ShapeType == "LogOn" || context.ShapeType == "Register") {
-                if ((HttpContext.Current.Session["parameters"] as Core.OpenAuthenticationParameters) != null)
+                if ((_orchardServices.WorkContext.HttpContext.Session["parameters"] as Core.OpenAuthenticationParameters) != null)
                     context.Shape.Metadata.Wrappers.Add("Wrappers_Account_AssociateMessage");
             }
         }
 
-        public static bool IsLogOn(ShapeCreatedContext context) {
+        public bool IsLogOn(ShapeCreatedContext context) {
             return context.ShapeType == "LogOn";
         }
 
-        public static bool IsCreate(ShapeCreatedContext context) {
-            if (context.ShapeType == "Create" && 
-                HttpContext.Current.Request.RequestContext.RouteData.Values["Controller"].ToString().Equals("Admin", StringComparison.InvariantCultureIgnoreCase) &&
-                HttpContext.Current.Request.RequestContext.RouteData.Values["Area"].ToString().Equals("NGM.OpenAuthentication", StringComparison.InvariantCultureIgnoreCase)) {
+        public bool IsCreate(ShapeCreatedContext context) {
+            if (context.ShapeType == "Create" &&
+                _orchardServices.WorkContext.HttpContext.Request.RequestContext.RouteData.Values["Controller"].ToString().Equals("Admin", StringComparison.InvariantCultureIgnoreCase) &&
+                _orchardServices.WorkContext.HttpContext.Request.RequestContext.RouteData.Values["Area"].ToString().Equals("NGM.OpenAuthentication", StringComparison.InvariantCultureIgnoreCase)) {
                 return true;
             }
             return false;
