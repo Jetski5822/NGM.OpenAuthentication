9e8ca9277b13950e8465d6d9971c1fcfedcd48d1
Nicholas Mayne <Jetski5822@gmail.com>
2012-03-16 09:44:27 +0000

    Removed backwards compatibility for lower then 1.0

--- Core/OrchardShapeChecker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NGM.OpenAuthentication.Core {
-    internal static class OrchardShapeChecker {
-        internal static bool HasRegisterAsShape() {
-            var version = new System.Reflection.AssemblyName(typeof (Orchard.ContentManagement.ContentItem).Assembly.FullName).Version.ToString();
-            var versionInt = int.Parse(version.Replace(".", string.Empty));
-            return versionInt > 10200;
-        }
-    }
-}
