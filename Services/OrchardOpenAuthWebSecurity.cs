1dc63882e0255cc57c61c21c03737c4c24fd0e6a
Nicholas Mayne <Jetski5822@gmail.com>
2013-06-09 13:26:47 +0100

    Upgrading packages and moving to encryptionservice.

--- Services/OrchardOpenAuthWebSecurity.cs
+++ Services/OrchardOpenAuthWebSecurity.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Security;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Web.Security;
 using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
 using NGM.OpenAuthentication.Models;
 using Orchard;
 using Orchard.Security;
@@ -20,13 +24,16 @@ namespace NGM.OpenAuthentication.Services {
         private readonly IOpenAuthSecurityManagerWrapper _openAuthSecurityManagerWrapper;
         private readonly IUserProviderServices _userProviderServices;
         private readonly IOrchardOpenAuthClientProvider _orchardOpenAuthClientProvider;
+        private readonly IEncryptionService _encryptionService;
 
         public OrchardOpenAuthWebSecurity(IOpenAuthSecurityManagerWrapper openAuthSecurityManagerWrapper,
                                           IUserProviderServices userProviderServices,
-                                          IOrchardOpenAuthClientProvider orchardOpenAuthClientProvider) {
+                                          IOrchardOpenAuthClientProvider orchardOpenAuthClientProvider,
+                                          IEncryptionService encryptionService) {
             _openAuthSecurityManagerWrapper = openAuthSecurityManagerWrapper;
             _userProviderServices = userProviderServices;
             _orchardOpenAuthClientProvider = orchardOpenAuthClientProvider;
+            _encryptionService = encryptionService;
         }
 
         public AuthenticationResult VerifyAuthentication(string returnUrl) {
@@ -55,18 +62,47 @@ namespace NGM.OpenAuthentication.Services {
             Argument.ThrowIfNullOrEmpty(providerName, "providerName");
             Argument.ThrowIfNullOrEmpty(providerUserId, "providerUserId");
 
-            return ProviderUserIdSerializationHelper.ProtectData(providerName, providerUserId);
+            var protectedBytes = ToByteArray(new SerializedProvider {ProviderName = providerName, ProviderUserId = providerUserId});
+            return Encoding.UTF8.GetString(_encryptionService.Encode(protectedBytes));
         }
 
 
         public bool TryDeserializeProviderUserId(string data, out string providerName, out string providerUserId) {
             Argument.ThrowIfNullOrEmpty(data, "data");
 
-            return ProviderUserIdSerializationHelper.UnprotectData(data, out providerName, out providerUserId);
+            var protectedBytes = _encryptionService.Decode(Encoding.UTF8.GetBytes(data));
+            var provider = (SerializedProvider) ToObject(protectedBytes);
+            providerName = provider.ProviderName;
+            providerUserId = provider.ProviderUserId;
+
+            return true;
         }
 
         public OrchardAuthenticationClientData GetOAuthClientData(string providerName) {
             return _orchardOpenAuthClientProvider.GetClientData(providerName);
+        }
+
+        [Serializable]
+        private struct SerializedProvider {
+            public string ProviderName { get; set; }
+            public string ProviderUserId { get; set; }
+        }
+
+        private byte[] ToByteArray(object source) {
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream()) {
+                formatter.Serialize(stream, source);
+                return stream.ToArray();
+            }
+        }
+
+        private object ToObject(byte[] source) {
+            var formatter = new BinaryFormatter();
+            using (var ms = new MemoryStream(source)) {
+                using (var ds = new DeflateStream(ms, CompressionMode.Decompress, true)) {
+                    return formatter.Deserialize(ds);
+                }
+            }
         }
     }
 }
